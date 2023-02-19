ca0278491c4676a3224a47225e1f3e8bf011de74
yonasketema <yonask.kitaw@gmail.com>
2023-02-19 14:42:36 +0300

    delivery Route Optimization

--- deliveryRouteOptimization.js
+++ deliveryRouteOptimization.js
@@ -1,39 +1,27 @@
-const buildings = [
-  {
-    name: "A",
-    distance: { B: 20, C: 12, D: 3 }
-  },
-  {
-    name: "B",
-    distance: { A: 20, C: 3, D: 12 }
-  },
-  {
-    name: "C",
-    distance: { A: 12, B: 3, D: 7 }
-  },
-  {
-    name: "D",
-    distance: { A: 3, B: 12, C: 7 }
-  }
-];
-
 function delivery_route_optimization(buildings, start) {
+  // create map of the buildings
   let points = {};
   for (const build of buildings) {
+    //points with building name and distance of each pair
     points[build.name] = build.distance;
   }
 
+  //store all visted buildings
   let visted = {};
   visted[start.name] = 1;
+  //final route
   let optimaize_route = [start.name];
   let i = 1;
 
   while (i < Object.keys(points).length) {
+    // current building
     let current = start.name;
+    // the next nearest building
     let nextBuild = null;
     let maxDistance = Number.POSITIVE_INFINITY;
 
     for (const build in points[current]) {
+      //finding the nearest unvisited building
       if (!visted[build]) {
         if (points[current][build] < maxDistance) {
           maxDistance = points[current][build];
@@ -42,21 +30,15 @@ function delivery_route_optimization(buildings, start) {
       }
     }
 
+    //assign the next closest building
     current = nextBuild;
     visted[nextBuild] = 1;
     optimaize_route.push(nextBuild);
 
     i++;
   }
-
+  optimaize_route.push(start.name);
   return optimaize_route;
 }
 
-let s = {
-  name: "A",
-  distance: { B: 20, C: 12, D: 3 }
-};
-
-let route = delivery_route_optimization(buildings, s);
-
-console.log(route);
+module.exports = delivery_route_optimization;
